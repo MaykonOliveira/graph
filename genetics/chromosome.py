39c114ff73e37cf00da279587626a2d3c39ff16c
maykon.oliveira <maykon.oliveira@neoway.com.br>
2018-12-08 19:44:23 -0200

    Added genetics base

---
+++ genetics/chromosome.py
@@ -0,0 +1,11 @@
+class Chromosome(object):
+
+    def __init__(self):
+        self._fitness = 0
+        self._genes = []
+
+    def define_genes(self, possible_genes):
+        pass
+
+    def calculate_fitness(self):
+        pass
---
+++ genetics/population.py
@@ -0,0 +1,7 @@
+class Population(object):
+
+    def __init__(self, graph):
+        self._graph = graph
+
+    def generate_initial_population(self):
+        pass
