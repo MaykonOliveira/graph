e8a952181f6bf4c0f12a7edb88dccbb3c9e66bd9
JoaoCastro <joaovitorsoaresc96@gmail.com>
2018-12-09 17:11:12 -0200

    mutation and selection

--- genetics/population.py
+++ genetics/population.py
@@ -1,5 +1,8 @@
 from genetics.chromosome import Chromosome
 
+import random
+import copy
+import operator
 import signal
 
 
@@ -38,3 +41,46 @@ class Population(object):
 
     def get_population(self):
         return self._population
+
+    def elitist_selection(self, elitist_treshold=0.75):
+        selected_population = []
+        
+        for i in range(self._population_size):
+            r = round(random.uniform(0, 1.0), 2)
+            sample_1,sample_2 = None, None
+
+            while sample_1 == sample_2:
+                sample_1 = random.choice(self._population)
+                sample_2 = random.choice(self._population)
+
+            if r =< elitist_treshold:
+                #choose best
+                survivor = min((sample_1,sample_2), key=operator.methodcaller('get_fitness'))
+            else:
+                #choose worst
+                survivor = max((sample_1,sample_2), key=operator.methodcaller('get_fitness'))
+
+            selected_population.append(copy.deepcopy(survivor))
+        
+        self._population = selected_population
+    
+
+    def crossover(self, crossover_rate=0.60):
+        pass
+
+    
+    def mutation(self, mutation_rate=0.01):
+        for chromosome in self._population:
+            for index, gene in enumerate(chromosome.get_genes()):
+                r = round(random.uniform(0, 100.0), 3)
+                rand_gene_index = None
+
+                if r <= mutation_rate:
+                    while rand_gene_index == index:
+                        rand_gene_index = random.randint(0, len(chromosome.get_genes())
+
+                    self._swap(gene[index], gene[rand_gene_index]) 
+
+
+    def _swap(item_1, item_2):
+        item_1, item_2 = item_2, item_1
